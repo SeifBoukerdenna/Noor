8b0f7dd0b41f2c5d4104b30348e52c94343f6c1c
Seif Boukerdenna <elmelz6472@gmail.com>
2025-11-06 21:57:36 -0500

    Initial Commit

---
+++ Noor/NoorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoorApp.swift
+//  Noor
+//
+//  Created by Malik Macbook on 2025-11-06.
+//
+
+import SwiftUI
+
+@main
+struct NoorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
